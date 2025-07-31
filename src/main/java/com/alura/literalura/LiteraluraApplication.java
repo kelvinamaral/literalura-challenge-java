f948ef6e22b0d1ad4797295dc81e038e31743a5d
Kelvin <kelvinfamaral@hotmail.com>
2025-07-30 23:12:43 -0300

    Alterações classe Principal

--- src/main/java/com/alura/literalura/LiteraluraApplication.java
+++ src/main/java/com/alura/literalura/LiteraluraApplication.java
@@ -1,21 +1,27 @@
 package com.alura.literalura;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import principal.Principal;
 
-import java.util.Scanner;
-
 @SpringBootApplication
 public class LiteraluraApplication implements CommandLineRunner {
+
+	@Autowired
+	private final Principal principal;
+
+	public LiteraluraApplication(Principal principal){
+		this.principal = principal;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
 	}
+
 	@Override
-	public void run(String... args) {
-		Principal principal = new Principal();
-		principal.exibeMenu();
+	public void run(String... args) throws Exception {
+		principal.exibirMenu();
 	}
 }
